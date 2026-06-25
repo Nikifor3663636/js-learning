6f1c9b7376b7e3628fbeedddf9516e9659aedc75
Nikifor3663636 <vakarenkonikita82@gmail.com>
2026-06-25 13:20:56 +0300

    Initial commit - add dweek1: add day1 practice - variables and calculationsay1.js

---
+++ week1/day1-practice.js
@@ -0,0 +1,4 @@
+const birthYear = 2003
+const currentYear = 2026
+const myAge = currentYear - birthYear
+console.log(`I was born in ${birthYear}, current year is ${currentYear}, my age is ${myAge}`)
